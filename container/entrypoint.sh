542888918c71453a01584f29fcba89cd0e5673fa
Ventz Petkov <ventz@vpetkov.net>
2019-05-04 02:51:14 -0400

    Hot Fix #1 - https://github.com/ventz/docker-bind/issues/18

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 OPTIONS=$@
-chown -R root:named /etc/bind /var/cache/bind /var/run/named
+chown -R root:named /etc/bind /var/run/named
+chown -R named:named /var/cache/bind
 chmod -R 770 /var/cache/bind /var/run/named
 chmod -R 750 /etc/bind
 # Run in foreground and log to STDERR (console):
