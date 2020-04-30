b2983bd53f9aee1b4bd9fe48700ab9f55a43fe56
Ventz Petkov <ventz@vpetkov.net>
2020-04-29 23:04:40 -0400

    TWO MAJOR CHANGES: Updated ROOT HINTS file from InterNIC as it was updated 4-29-2020. Also changed the permissions both build and entrypoint (run-time change) to match least permissions needed. This should tighten up the permissions for dynamically generated zones

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 OPTIONS=$@
+# "Run Time" changes - needed for when creating a *new* directory/first-time volume map
+# A great example of this is "/var/cache/bind" for dynamic configs, and mapping it in
+# The first time around, it will not be owned by named:named, and thus it won't be writable
 chown -R root:named /etc/bind /var/run/named
 chown -R named:named /var/cache/bind
-chmod -R 770 /var/cache/bind /var/run/named
+chmod 770 /var/cache/bind /var/run/named
 chmod -R 750 /etc/bind
 # By default - run in foreground and log to STDERR (console)
 # can be changed by running container with: -e "BIND_LOG=-f"
